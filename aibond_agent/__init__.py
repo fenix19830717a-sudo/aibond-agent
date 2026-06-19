6bf1e449fc7a329afc421814a7624931db955921
fenix19830717a-sudo <fenix19830717a@gmail.com>
2026-06-19 10:32:22 +0800

    fix: SDK improvements - on_connected callback, runtime fixes, private message support

--- aibond_agent/__init__.py
+++ aibond_agent/__init__.py
@@ -1,6 +1,7 @@
 """Aibond Agent SDK."""
 
 from aibond_agent.client import AibondClient
+from aibond_agent.agent_runtime import AgentRuntime, SkillRegistry
 
 __version__ = "0.1.0"
-__all__ = ["AibondClient"]
+__all__ = ["AibondClient", "AgentRuntime", "SkillRegistry"]
